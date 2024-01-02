fa5881886ef00ac3f9028b16edf7d732340a8aba
wangjiansheng <wangjiansheng_sl@163.com>
2024-01-02 18:57:20 +0800

    读完第4章数组

--- PHP 8 从入门到精通/04_数组/04_数组的操作.php
+++ PHP 8 从入门到精通/04_数组/04_数组的操作.php
@@ -49,5 +49,57 @@ print_r($clients);
   */
 
   /**
+   * 调换键和值
+   * array_flip()
+   */
+  $arr_1 = array(
+    '单人间' => 188,
+    '标准间' => 288,
+    '大床房' => 388,
+    '家庭房' => 488
+  );
+  print_r(array_flip($arr_1));
+
+  /**
+   * PHP 8 新特性
+   * 1.引用赋值时，自动创建的数组元素顺序变化
+   * 2.list()函数不再按照相反的顺序赋值,不允许空值,不支持字符串拆分,可用于数组对象
    * 
    */
+list($array[],$array[],$array[]) = [100,200,300];
+var_dump($array);
+
+
+/**
+ * foreach()修改数组
+ * 1.对内部指针失效, current($array) 不会变化
+ * 2.按值循环不影响循环
+ * 3.按引用循环会影响循环
+ */
+foreach($array as $arr){
+  unset($array[1]);
+  echo "数值为 $arr \n";
+}
+
+foreach($array as &$arr){
+  echo "数值为 $arr \n";
+  $array[1] = 300;
+  $array[2] = 400;
+}
+
+/**
+ * 序列化与反序列化
+ * serialize()
+ * unserialize()
+ */
+echo serialize($arr_1)."\n";
+
+/**
+ * 数组合并
+ * array_merge()
+ */
+
+ /**
+  * 快速清空数组
+  * unset($array)
+  */
